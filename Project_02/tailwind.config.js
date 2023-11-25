fabe599edb033878af16c497b6be21cd21978a44
Prabhat Srivastava <srivastavaprabhat522@gmail.com>
2023-11-25 22:17:11 +0530

    Add files via upload

---
+++ Project_02/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      screen:{
+        "sm":"480px"
+      }
+    },
+    fontFamily:{
+      nunito:['nunito', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
+
